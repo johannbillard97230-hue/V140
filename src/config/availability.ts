a590fdbaeb652dc0f0865b8bfb75f4d67359cc46
johannbillard97230-hue <johannbillard97230@gmail.com>
2026-09-08 12:07:50 +0200

    DISPO

--- src/config/availability.ts
+++ src/config/availability.ts
@@ -21,12 +21,11 @@ export const augustAllowedRanges = [
   { from: new Date(2026, 7, 5), to: new Date(2026, 7, 5) },
 ];
 
-// September 2026 availability: 1, 6, 13, 15, 24, 30 (blocks 2-5, 7-12, 14, 16-23, 25-29)
+// September 2026 availability: 1, 6, 13-21, 24, 30 (blocks 2-5, 7-12, 22-23, 25-29)
 export const septemberAllowedRanges = [
   { from: new Date(2026, 8, 1), to: new Date(2026, 8, 1) },
   { from: new Date(2026, 8, 6), to: new Date(2026, 8, 6) },
-  { from: new Date(2026, 8, 13), to: new Date(2026, 8, 13) },
-  { from: new Date(2026, 8, 15), to: new Date(2026, 8, 15) },
+  { from: new Date(2026, 8, 13), to: new Date(2026, 8, 21) },
   { from: new Date(2026, 8, 24), to: new Date(2026, 8, 24) },
   { from: new Date(2026, 8, 30), to: new Date(2026, 8, 30) },
 ];
